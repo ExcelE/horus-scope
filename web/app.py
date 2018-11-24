1704320e6dd8242bf848196d4017b43bbb2e5c37
ExcelE <excelespina@gmail.com>
2018-11-24 18:39:39 -0500

    C:/Program Files/Git/refill done

--- web/app.py
+++ web/app.py
@@ -157,18 +157,14 @@ class Classify(Resource):
 
 class Refill(Resource):
     def post(self):
-        postedData = request.get_json()
+        
+        username = request.form['username']
+        password = request.form['password']
+        amount = request.form['amount'] or 3
 
-        username = postedData["username"]
-        password = postedData["admin_pw"]
-        amount = postedData["amount"]
-
-        if not UserExist(username):
-            return jsonify(generateReturnDictionary(301, "Invalid Username"))
-
-        correct_pw = "abc123"
-        if not password == correct_pw:
-            return jsonify(generateReturnDictionary(302, "Incorrect Password"))
+        retJson, error = verifyCredentials(username, password)
+        if error:
+            return retJson, retJson['status']
 
         users.update({
             "Username": username
@@ -177,7 +173,12 @@ class Refill(Resource):
                 "Tokens": amount
             }
         })
-        return jsonify(generateReturnDictionary(200, "Refilled"))
+
+        return {
+            "status": 200,
+            "msg": "Refilled",
+            "amount": amount
+        }, 200
 
 
 class Login(Resource):
@@ -214,4 +215,3 @@ api.add_resource(Refill, '/refill')
 
 if __name__=="__main__":
     app.run(host='0.0.0.0', debug=True)
-
