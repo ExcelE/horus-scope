08bbd9fed31576f710a61843f019baaf584da484
ExcelE <excelespina@gmail.com>
2018-11-30 20:51:41 -0500

    REST_Framework

--- src/argus/urls.py
+++ src/argus/urls.py
@@ -21,5 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('classifier.urls'))
+    path('api/', include('classifier.urls')),
+    url(r'^api-auth/', include('rest_framework.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
